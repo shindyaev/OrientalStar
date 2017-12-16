11286fdcb0065e01b4bb68b141bb59d873e3192a
Shindyaev <serg.shindyaev@me.com>
2017-12-16 13:38:21 +0400

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shoptest.dev/admin/');
+define('HTTP_CATALOG', 'http://shoptest.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shoptest.dev/admin/');
+define('HTTPS_CATALOG', 'http://shoptest.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/shoptest.dev/admin/');
+define('DIR_SYSTEM', '/var/www/shoptest.dev/system/');
+define('DIR_IMAGE', '/var/www/shoptest.dev/image/');
+define('DIR_LANGUAGE', '/var/www/shoptest.dev/admin/language/');
+define('DIR_TEMPLATE', '/var/www/shoptest.dev/admin/view/template/');
+define('DIR_CONFIG', '/var/www/shoptest.dev/system/config/');
+define('DIR_CACHE', '/var/www/shoptest.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/shoptest.dev/system/storage/download/');
+define('DIR_LOGS', '/var/www/shoptest.dev/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/shoptest.dev/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/shoptest.dev/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/shoptest.dev/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oriental');
+define('DB_PASSWORD', 'Ufg56dFtyUrt');
+define('DB_DATABASE', 'oriental');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shoptest.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shoptest.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/shoptest.dev/catalog/');
+define('DIR_SYSTEM', '/var/www/shoptest.dev/system/');
+define('DIR_IMAGE', '/var/www/shoptest.dev/image/');
+define('DIR_LANGUAGE', '/var/www/shoptest.dev/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/shoptest.dev/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/shoptest.dev/system/config/');
+define('DIR_CACHE', '/var/www/shoptest.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/shoptest.dev/system/storage/download/');
+define('DIR_LOGS', '/var/www/shoptest.dev/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/shoptest.dev/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/shoptest.dev/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oriental');
+define('DB_PASSWORD', 'Ufg56dFtyUrt');
+define('DB_DATABASE', 'oriental');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
